cc9c01503b702901e2bd32990c08de3302899040
yunlong <laureler@163.com>
2019-01-10 17:41:26 +0800

    删除cmd

--- book.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\gitbook-summary\bin\summary.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\gitbook-summary\bin\summary.js" %*
-)
